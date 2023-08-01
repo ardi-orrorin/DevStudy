144a472a0591b9778277b24509b833b8fdf1d95f
yoosc89 <yoosc89@gmail.com>
2023-08-01 22:48:13 +0900

    stomp kafka 연동 선공

--- Platform/Kafka/99studyProject/02springboot/init/src/main/java/com/kafka/init/config/WebSocketConfig.java
+++ Platform/Kafka/99studyProject/02springboot/init/src/main/java/com/kafka/init/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.kafka.init.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 //        registry.addEndpoint("/stomp")
@@ -19,14 +22,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 //                .setDisconnectDelay(30 * 1000);
 
 //        registry.addEndpoint("/websocket");
-//        registry.addEndpoint("/websocket").setAllowedOrigins("*").setAllowedOriginPatterns("*").withSockJS();
-        registry.addEndpoint("/websocket").withSockJS();
+//        registry.addEndpoint("/websocket")
+//                .setAllowedOrigins("*")
+//                .setAllowedOriginPatterns("*")
+//                .withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/pub"); // borker 역할 수행시 사용할 prefix
-//        registry.enableSimpleBroker("/subscribe", "/topic");
-        registry.setApplicationDestinationPrefixes("/sub"); // 메시지 수신용 prefix
+        registry.enableSimpleBroker("/sub"); // sub 역할 수행시 사용할 prefix
+        registry.setApplicationDestinationPrefixes("/pub"); // 메시지 발신용 prefix
     }
+
 }
--- Platform/Kafka/99studyProject/02springboot/init/src/main/java/com/kafka/init/controller/GreetingController.java
+++ Platform/Kafka/99studyProject/02springboot/init/src/main/java/com/kafka/init/controller/GreetingController.java
@@ -1,16 +1,64 @@
 package com.kafka.init.controller;
+import com.jayway.jsonpath.internal.function.sequence.First;
+import com.kafka.init.dto.MessageDTO;
 import com.kafka.init.dto.TestDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.broker.SimpleBrokerMessageHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-@Controller
+import java.util.concurrent.CompletableFuture;
+
+
+@RestController
+@RequiredArgsConstructor
 public class GreetingController {
 
-    @MessageMapping("/message")
-//    @SendTo("/pub/message")
-    public TestDTO message(TestDTO message){
-        System.out.println("message = " + message);
-        return message;
+    private final KafkaTemplate<String , MessageDTO> kafkaTemplate;
+    private final NewTopic myTopic1;
+
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+    
+    @EventListener
+    public void webSocketConnect(SessionConnectEvent event){
+        System.out.println("event = " + event);
     }
+    
+    @EventListener
+    public void webSocketDisConnect(SessionDisconnectEvent event){
+        System.out.println("event = " + event);
+    }
+
+    @MessageMapping("/message/{room}") // setApplicationDestinationPrefixes
+//    @SendTo("/sub/topic/{id}") // enableSimpleBroker
+    public void message(@DestinationVariable String room, MessageDTO message){
+        message.setRoom(room);
+        System.out.println("message = " + message);
+        CompletableFuture<SendResult<String ,MessageDTO>> future =
+                kafkaTemplate.send(myTopic1.name(), message);
+    }
+
+    @KafkaListener(topics = "topic01" ,groupId = "foo")
+    public void subMessage(MessageDTO messageDTO){
+        System.out.println("messageDTO = " + messageDTO);
+
+        simpMessageSendingOperations.convertAndSend("/sub/message/"+messageDTO.getRoom(),messageDTO);
+    }
+
+
+
 }
