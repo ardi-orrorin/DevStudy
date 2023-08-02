55fb167abee106f742a473e4e13c52fd63803933
yoosc89 <yoosc89@gmail.com>
2023-08-02 20:40:09 +0900

    ..

--- Project/INFOMATE-Kafka&MongoDB/chat/src/main/java/com/infomate/chat/repository/ChatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infomate.chat.repository;
-
-import com.infomate.chat.entity.Message;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatRepository extends MongoRepository<Message, Integer> {
-}
