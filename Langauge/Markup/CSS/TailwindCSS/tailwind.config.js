1f85317dcaf595b009cef0c682347bff0ab8b51d
yoosc89 <yoosc89@gmail.com>
2023-12-02 16:06:19 +0900

    typography 내용 정리겸 학습

---
+++ Langauge/Markup/CSS/TailwindCSS/tailwind.config.js
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'notoSansKr': ["'Noto Sans KR'", 'sans-serif'],
+        'nanumGothic': ["'Nanum Gothic'", 'sans-serif'],
+      },
+      fontSize: {
+        'customSm': '1.2rem',
+        'customBase': '1.4rem',
+        'customXL': '1.6rem',
+        'custom2XL': '2rem',
+        'customSmD': ['1.2rem', '2rem'],
+        'customBaseD': ['1.4rem', '2.5rem'],
+        'customXLD': ['1.6rem', '3rem'],
+        'custom2XLD': ['2rem', '3.5rem'],
+        'customXl-LLF': ['1.5rem', {
+          lineHeight: '2rem',
+          letterSpacing: '-0.2em',
+          fontWeight: '500',
+        }],
+        'custom2Xl-LLF': ['1.875rem', {
+          lineHeight: '2.25rem',
+          letterSpacing: '1em',
+          fontWeight: '800',
+        }],
+      },
+      lineClamp: {
+        7: '7',
+      }, 
+      lineHeight: {
+        'extra-loose': '2.5',
+        '12': '3rem',
+      },
+      listStyleImage: {
+        listImage: 'url("03_list/check.png")',
+      },
+      colors: {
+        'regal-blue': '#243c5a',
+      },
+      spacing: {
+        'cusSpacing-128': '32rem',
+      },
+      textIndent: {
+        'cusIndent-128': '32rem',
+      }
+    },
+  },
+  plugins: [],
+};
