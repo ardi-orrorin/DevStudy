28a6fcdbe3e6f6dced8b8083ee9994763c9fd611
yoosc89 <yoosc89@gmail.com>
2023-12-24 13:32:54 +0900

    update

--- Langauge/Swift/framework/uikit/helloworld/helloworld/ViewController.swift
+++ Langauge/Swift/framework/uikit/helloworld/helloworld/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var blbLabel: UILabel!
+    
+    @IBOutlet var txtNameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSend(_ sender: UIButton) {
+        blbLabel.text = "this is \(txtNameField.text!)"
+    }
+    
 }
 
