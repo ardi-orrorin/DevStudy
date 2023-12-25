379b81df352530481c8d4c1a2e1f4303ec893c7d
yoosc89 <yoosc89@gmail.com>
2023-12-25 15:11:23 +0900

    uikit imageview

--- Langauge/Swift/framework/uikit/ImageView/ImageView/ViewController.swift
+++ Langauge/Swift/framework/uikit/ImageView/ImageView/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "picachu-color.png")
+        imgOff = UIImage(named: "picachu-black.png")
+        
+        imgView.image = imgOn
     }
 
-
+    @IBAction func btnResizeImg(_ sender: UIButton) {
+        let scale: CGFloat = 2.0
+        var newWidth: CGFloat, newHeight: CGFloat
+        
+        if isZoom {
+            newWidth = imgView.frame.width / scale
+            newHeight = imgView.frame.height / scale
+            btnResize.setTitle("Zoom Out", for: .normal)
+        } else {
+            newWidth = imgView.frame.width * scale
+            newHeight = imgView.frame.height * scale
+            btnResize.setTitle("Zoom In", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        
+        isZoom = !isZoom
+    }
+    @IBAction func switchImgToggle(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
+    
 }
 
