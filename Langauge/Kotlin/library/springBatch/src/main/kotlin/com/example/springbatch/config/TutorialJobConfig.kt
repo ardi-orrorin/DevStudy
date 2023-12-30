f76c2a632e5c2d4e64b06b0ae8243b79cce03481
yoosc89 <yoosc89@gmail.com>
2023-12-30 12:42:06 +0900

    update

--- Langauge/Kotlin/library/springBatch/src/main/kotlin/com/example/springbatch/config/TutorialJobConfig.kt
+++ Langauge/Kotlin/library/springBatch/src/main/kotlin/com/example/springbatch/config/TutorialJobConfig.kt
@@ -1,4 +1,39 @@
 package com.example.springbatch.config
 
-class TutorialJobConfig {
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+@EnableBatchProcessing
+class TutorialJobConfig{
+
+    @Bean
+    fun helloWorldJob(jobRepository: JobRepository): Job {
+        return JobBuilder("helloWorldJob", jobRepository)
+            .start(helloWorldStep(jobRepository))
+            .build()
+    }
+
+    @Bean
+    fun helloWorldStep(jobRepository: JobRepository): Step {
+        return StepBuilder("helloWorldStep", jobRepository)
+            .tasklet {
+                contribution: StepContribution, chunkContext: ChunkContext ->
+                println("Hello, World!")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
 }
