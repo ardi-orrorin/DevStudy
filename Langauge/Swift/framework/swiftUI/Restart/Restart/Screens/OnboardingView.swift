dc8bcecf6ad1e507ca7be9eb81a2b063f1472e6e
yoosc89 <yoosc89@gmail.com>
2024-01-18 21:43:00 +0900

    ios project onboardingView layout design update

--- Langauge/Swift/framework/swiftUI/Restart/Restart/Screens/OnboardingView.swift
+++ Langauge/Swift/framework/swiftUI/Restart/Restart/Screens/OnboardingView.swift
@@ -12,16 +12,105 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingVeiwActive: Bool = true
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action : {
-                isOnboardingVeiwActive = false
-            }) {
-                Text("Start")
-            }
-        } //: VStack
+            VStack(spacing: 20) {
+                // MARK: HEDAER
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                        It's not how much we give but
+                        how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                    
+                    
+                } //: HEADER
+                
+                // MARK: CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    } //: ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                
+                Spacer()
+            
+                // MARK: FOOTER
+                ZStack {
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    Text("Get Started")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingVeiwActive = false
+                        }
+                        
+                        Spacer()
+                    } //: HStack
+                    
+                    
+                    
+                } //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
+                
+                
+            } //: VStack
+        } //: ZStack
     }
 }
 
