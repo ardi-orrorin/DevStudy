4d162cee928c448b7a6e96e8cafa4b61d7e46106
yoosc89 <yoosc89@gmail.com>
2024-01-19 23:08:02 +0900

    update

--- Langauge/Swift/framework/swiftUI/Restart/Restart/Screens/ContentView.swift
+++ Langauge/Swift/framework/swiftUI/Restart/Restart/Screens/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
+            
             if isOnboardingVeiwActive {
                 OnboardingView()
             } else {
