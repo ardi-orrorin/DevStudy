d0899363780a1aeefea13b1c923d68439d8a94ae
yoosc89 <yoosc89@gmail.com>
2024-01-25 21:55:11 +0900

    create section 11 prj

---
+++ Langauge/Swift/framework/swiftUI/TouchDown/TouchDown/App/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by 유승철 on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Langauge/Swift/framework/swiftUI/TouchDown/TouchDown/Utility/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  TouchDown
+//
+//  Created by 유승철 on 1/25/24.
+//
+
+import SwiftUI
+
+// data
+
+// color
+let COLOR_BACKGROUND: Color = Color("ColorBackgrounnd")
+let COLOR_GRAU: Color = Color(UIColor.systemGray4)
+
+// layout
+
+// ux
+
+// api
+
+// image
+
+// font
+
+// string
+
+// misc
