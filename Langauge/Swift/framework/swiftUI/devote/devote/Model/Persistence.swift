d7ee9bbe772ece89a5e8823f4d55e23793db65be
yoosc89 <yoosc89@gmail.com>
2024-01-29 21:39:26 +0900

    coreData

---
+++ Langauge/Swift/framework/swiftUI/devote/devote/Model/Persistence.swift
@@ -0,0 +1,44 @@
+//
+//  Persistence.swift
+//  devote
+//
+//  Created by 유승철 on 1/29/24.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "devote")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        for _ in 0..<10 {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+}
---
+++ Langauge/Swift/framework/swiftUI/devote/devote/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  devote
+//
+//  Created by 유승철 on 1/29/24.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+
+// MARK: - ui
+
+
+// MARK: - ux
