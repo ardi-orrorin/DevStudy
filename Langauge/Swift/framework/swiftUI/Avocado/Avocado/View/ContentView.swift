f3fb5a44e1e9a31d66e989282c4ed5dd2321292a
yoosc89 <yoosc89@gmail.com>
2024-02-04 20:19:27 +0900

    contents view update

--- Langauge/Swift/framework/swiftUI/Avocado/Avocado/View/ContentView.swift
+++ Langauge/Swift/framework/swiftUI/Avocado/Avocado/View/ContentView.swift
@@ -8,31 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            VStack {
+            VStack(alignment: .center, spacing: 20) {
                 // MARK: - header
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(alignment: .top, spacing: 0) {
                         ForEach(headerDatas) { header in
                             HeaderView(header: header)
+                        } //: loop
+                    }//: hstack
+                }//: scrollview
+                
+                // MARK: - dishes
+                
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                DishesView()
+                    .frame(maxWidth: 640)
+                
+                // MARK: - avocado facts
+                
+                Text("Avocado Facts")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 60) {
+                        ForEach(FactDatas) { fact in
+                            FactsView(fact: fact)
                         }
                     }
+                    .padding(.vertical)
+                    .padding(.leading, 60)
+                    .padding(.trailing, 20)
                 }
-                
                 
                 // MARK: - footer
                 VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
                     Text("All about Avocados")
-                        .font(.system(.title, design: .serif))
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("ColorGreenAdaptive"))
-                        .padding(8)
+                        .modifier(TitleModifier())
                     
                     Text("Everything ypu wantyed to know about avocados but were too afraid to ask.")
                         .font(.system(.body, design: .serif))
                         .multilineTextAlignment(.center)
                         .foregroundColor(.gray)
+                        .frame(minHeight: 60)
                 }//: VStack
                 .frame(maxWidth: 640)
                 .padding()
@@ -40,6 +66,16 @@ struct ContentView: View {
             }//: Vstack
         }//: ScrollView
         .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.title, design: .serif))
+            .fontWeight(.bold)
+            .foregroundColor(Color("ColorGreenAdaptive"))
+            .padding(8)
     }
 }
 
