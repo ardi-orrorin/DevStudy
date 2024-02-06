d863842d35f9934a931e5340445c77d0c9abe297
yoosc89 <yoosc89@gmail.com>
2024-02-06 21:44:39 +0900

    update

--- Langauge/Swift/framework/swiftUI/Avocado/Avocado/View/RipeningStagesView.swift
+++ Langauge/Swift/framework/swiftUI/Avocado/Avocado/View/RipeningStagesView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct RipeningStagesView: View {
+    
+    
     var body: some View {
-        Text("Ripening")
+        ScrollView(.horizontal, showsIndicators: false) {
+            VStack {
+                Spacer()
+                
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 25) {
+                    ForEach(ripeningData) { ripening in
+                        RipeningView(ripening: ripening)
+                    } //: loop
+                } //: hstack
+                .padding(.vertical)
+                .padding(.horizontal, 25)
+                
+                Spacer()
+            } //: vstack
+        } //: scrollView
     }
 }
 
--- Langauge/Swift/framework/swiftUI/Avocado/Avocado/View/RipeningView.swift
+++ Langauge/Swift/framework/swiftUI/Avocado/Avocado/View/RipeningView.swift
@@ -11,11 +11,13 @@ struct RipeningView: View {
     
     // MARK: - properties
     
+    var ripening: Ripening
+    
     @State private var slideInAnimation: Bool = false
     
     var body: some View {
         VStack {
-            Image("avocado-ripening-1")
+            Image(ripening.image)
                 .resizable()
                 .frame(width: 100, height: 100, alignment: .center)
                 .clipShape(Circle())
@@ -35,7 +37,7 @@ struct RipeningView: View {
             VStack(alignment: .center, spacing: 10) {
                 // stage
                 VStack(alignment: .center, spacing: 0) {
-                    Text("1")
+                    Text(ripening.stage)
                         .font(.system(.largeTitle, design: .serif))
                         .fontWeight(.bold)
                     Text("STAGE")
@@ -47,7 +49,7 @@ struct RipeningView: View {
                 .frame(width: 180)
                 
                 // title
-                Text("Hard")
+                Text(ripening.title)
                     .font(.system(.title, design: .serif))
                     .fontWeight(.bold)
                     .foregroundColor(Color("ColorGreenMedium"))
@@ -78,14 +80,14 @@ struct RipeningView: View {
                 
                 // description
                 Spacer()
-                Text("Fresh off the tree, the avocado is very hard with no give")
+                Text(ripening.description)
                     .foregroundColor(Color("ColorGreenDark"))
                     .fontWeight(.bold)
                     .lineLimit(nil)
                 Spacer()
                 
                 // ripeness
-                Text("5+ DAYS")
+                Text(ripening.ripeness)
                     .foregroundColor(Color.white)
                     .font(.system(.callout, design: .serif))
                     .fontWeight(.bold)
@@ -112,7 +114,7 @@ struct RipeningView: View {
                             )
                     )
                 // instruction
-                Text("Hold avocados at room temperature until they are fully ripe. ")
+                Text(ripening.instruction)
                     .font(.footnote)
                     .foregroundColor(Color("ColorGreenLight"))
                     .fontWeight(.bold)
@@ -151,5 +153,5 @@ struct RipeningView: View {
 }
 
 #Preview {
-    RipeningView()
+    RipeningView(ripening: ripeningData[2])
 }
