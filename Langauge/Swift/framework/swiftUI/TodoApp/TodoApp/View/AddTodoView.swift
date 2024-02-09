956827192684927597ce7748efb8b36c92fab3fa
yoosc89 <yoosc89@gmail.com>
2024-02-09 14:37:28 +0900

    update

--- Langauge/Swift/framework/swiftUI/TodoApp/TodoApp/View/AddTodoView.swift
+++ Langauge/Swift/framework/swiftUI/TodoApp/TodoApp/View/AddTodoView.swift
@@ -29,49 +29,64 @@ struct AddTodoView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Form {
-                    // MARK: - todo name
-                    TextField("Todo", text: $name)
-                    
-                    
-                    // MARK: - todo priority
-                    Picker("Priority", selection: $priority) {
-                        ForEach(priorities, id: \.self) {
-                            Text($0)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                    
-                    // MARK: - save button
-                    HStack {
-                        Spacer()
-                        Button {
-                            if name != "" {
-                                let todo = Todo(context: managedObjectContext)
-                                todo.name = name
-                                todo.priority = priority
-                                
-                                do {
-                                    try managedObjectContext.save()
-                                } catch {
-                                    print(error)
-                                }
-                                
-                                print("New todo item: \(todo.name ?? ""), Priority: \(todo.priority ?? "")")
-                            } else {
-                                errorShowing = true
-                                errorTitle = "Invalid Name"
-                                errorMessage = "Make sure to enter something for\nthe new todo item."
-                                return
+                VStack(alignment: .leading, spacing: 20) {
+                    Form {
+                        // MARK: - todo name
+                        TextField("Todo", text: $name)
+                            .padding()
+                            .background(
+                                Color(UIColor.tertiarySystemFill)
+                            )
+                            .cornerRadius(9)
+                            .font(.system(size: 20, weight: .bold, design: .default))
+                        
+                        
+                        // MARK: - todo priority
+                        Picker("Priority", selection: $priority) {
+                            ForEach(priorities, id: \.self) {
+                                Text($0)
                             }
-                            presentationMode.wrappedValue.dismiss()
-                        } label: {
-                            Text("Save")
-                        }//: save button
-                        Spacer()
-                    }
-                }//: form
-                
+                        }
+                        .pickerStyle(.segmented)
+                        
+                        // MARK: - save button
+                        HStack {
+                            Spacer()
+                            Button {
+                                if name != "" {
+                                    let todo = Todo(context: managedObjectContext)
+                                    todo.name = name
+                                    todo.priority = priority
+                                    
+                                    do {
+                                        try managedObjectContext.save()
+                                    } catch {
+                                        print(error)
+                                    }
+                                    
+                                    print("New todo item: \(todo.name ?? ""), Priority: \(todo.priority ?? "")")
+                                } else {
+                                    errorShowing = true
+                                    errorTitle = "Invalid Name"
+                                    errorMessage = "Make sure to enter something for\nthe new todo item."
+                                    return
+                                }
+                                presentationMode.wrappedValue.dismiss()
+                            } label: {
+                                Text("Save")
+                            }//: save button
+                            .font(.system(size: 20, weight: .bold, design: .default))
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(.blue)
+                            .cornerRadius(9)
+                            .foregroundColor(.white)
+                            
+                            Spacer()
+                        } //: hstack
+                        .padding(.horizontal)
+                    } //: form
+                } //: vstack
                 Spacer()
             } //: Vstack
             .navigationBarTitle("New Todo", displayMode: .inline)
@@ -85,6 +100,7 @@ struct AddTodoView: View {
                 Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
             }
         } //: navigation
+    
         
     }
 }
