0023056383ff857d2bed48c8bb3a1d1908117cfa
yoosc89 <yoosc89@gmail.com>
2024-02-09 16:16:05 +0900

    section 18 init

---
+++ Langauge/Swift/framework/swiftUI/Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by 유승철 on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
