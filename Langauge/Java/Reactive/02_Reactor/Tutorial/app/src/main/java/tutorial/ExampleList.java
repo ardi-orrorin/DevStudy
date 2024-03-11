a501f74321edd27f7ff68304df31e5406eea0e5b
yoosc89 <yoosc89@gmail.com>
2024-03-11 21:41:13 +0900

    webflux 기본 내용 추가

---
+++ Langauge/Java/Reactive/02_Reactor/Tutorial/app/src/main/java/tutorial/ExampleList.java
@@ -0,0 +1,44 @@
+package tutorial;
+
+import java.time.Duration;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class ExampleList {
+    public static void main(String[] args) throws Exception {
+        // Flux.concat(
+        // Flux.just("one", "two", "three"),
+        // Flux.just("four", "five", "six"),
+        // Flux.just("seven", "eight", "nine"))
+        // .collectList()
+        // .subscribe(System.out::println);
+
+        // Flux<String> flux1 = Flux.just("one", "two", "three");
+        // flux1.log().subscribe();
+
+        // System.out.println("sdfsdf");
+
+        // flux1.log().subscribe();
+
+        // Flux<String> flux2 = Flux.just("four", "five", "six")
+        // .delayElements(Duration.ofSeconds(1))
+        // .share();
+
+        // flux2.subscribe(e -> System.out.println("main 1 : " + e));
+
+        // Thread.sleep(2000);
+
+        // flux2.subscribe(e -> System.out.println("main 2 : " + e));
+
+        // Thread.sleep(2500);
+
+        Mono<String> mono = Mono.just("one").map(e -> System.nanoTime() + "")
+                .cache();
+
+        mono.subscribe(e -> System.out.println("main 1 : " + e));
+        Thread.sleep(3000);
+        mono.subscribe(e -> System.out.println("main 2 : " + e));
+
+    }
+}
--- README.md
+++
@@ -1,6 +0,0 @@
-# 개인 공부용 GIT REPOSITORY
-
-개발 공부용 레포지토리 입니다.     
-
-추후 정리된 내용은 [orrorin.store](https://orrorin.store) 포트폴리오 페이지에 올라갑니다.  
-정리된 내용은 [Repo](https://github.com/yoosc89/Portfolio) 에서 확인 가능합니다.
