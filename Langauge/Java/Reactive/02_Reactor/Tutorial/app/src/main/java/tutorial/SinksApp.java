11d2f26cd6bec14b5a71acc93d5589653b68e2e2
yoosc89 <yoosc89@gmail.com>
2024-03-12 23:00:09 +0900

    Sinks 정리

--- Langauge/Java/Reactive/02_Reactor/Tutorial/app/src/main/java/tutorial/SinksApp.java
+++ Langauge/Java/Reactive/02_Reactor/Tutorial/app/src/main/java/tutorial/SinksApp.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 import org.checkerframework.checker.units.qual.s;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
@@ -52,30 +53,61 @@ public class SinksApp {
 
         // Thread.sleep(50L);
 
-        Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
-        int multi = 10;
-        for (int i = 0; i < multi; i++) {
-            new Thread() {
-                public void run() {
-                    String name = Thread.currentThread().getName();
-                    sink.emitNext("success" + name, Sinks.EmitFailureHandler.FAIL_FAST);
-                    System.out.println("success" + name);
-                }
-            }.start();
-            Thread.sleep(10L);
+        // Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+        // int multi = 10;
+        // for (int i = 0; i < multi; i++) {
+        // new Thread() {
+        // public void run() {
+        // String name = Thread.currentThread().getName();
+        // sink.emitNext("success" + name, Sinks.EmitFailureHandler.FAIL_FAST);
+        // // Sinks.EmitFailureHandler.FAIL_FAST 에러 발생 시 재시도 하지 않고 즉시 종료
+        // System.out.println("success" + name);
+        // }
+        // }.start();
+        // Thread.sleep(10L);
 
-        }
+        // }
 
+        // Flux<String> flux = sink.asFlux();
+
+        // flux
+        // // .fromIterable(List.of("Hello", "World", "/", "df"))
+        // .publishOn(Schedulers.parallel())
+        // .map(result -> result + "success")
+        // .doOnNext(result -> System.out.println(result))
+        // .subscribeOn(Schedulers.parallel())
+        // .subscribe();
+
+        // Thread.sleep(1000L);
+
+        // Sinks.One<String> sink = Sinks.one();
+        // Mono<String> mono = sink.asMono();
+
+        // sink.emitValue("success1", Sinks.EmitFailureHandler.FAIL_FAST);
+        // sink.emitValue("success2", Sinks.EmitFailureHandler.FAIL_FAST); // dropped
+
+        // mono.subscribe(System.out::println);
+        // mono.subscribe(System.out::println);
+
+
+        // Sinks.Many<String> sink = Sinks.many().unicast().onBackpressureBuffer(); //
+        // 하나의 subscriber만 처리
+        // Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer(); //
+        // 여러 subscriber 처리
+        Sinks.Many<String> sink = Sinks.many().replay().limit(2);
         Flux<String> flux = sink.asFlux();
 
-        flux
-                // .fromIterable(List.of("Hello", "World", "/", "df"))
-                .publishOn(Schedulers.parallel())
-                .map(result -> result + "success")
-                .doOnNext(result -> System.out.println(result))
-                .subscribeOn(Schedulers.parallel())
-                .subscribe();
+        sink.emitNext("success1", Sinks.EmitFailureHandler.FAIL_FAST);
+        sink.emitNext("success2", Sinks.EmitFailureHandler.FAIL_FAST);
+        sink.emitNext("success3", Sinks.EmitFailureHandler.FAIL_FAST);
 
-        Thread.sleep(1000L);
+        flux.subscribe(data -> System.out.println("subscriber 1 : " + data));
+
+        sink.emitNext("success4", Sinks.EmitFailureHandler.FAIL_FAST);
+
+        flux.subscribe(data -> System.out.println("subscriber 2 : " + data));
+
+        sink.emitNext("success5", Sinks.EmitFailureHandler.FAIL_FAST);
+
     }
 }
