756c71be39e2251715fccedbb5c710887bf154e2
yoosc89 <yoosc89@gmail.com>
2024-03-15 15:14:35 +0900

    webflux test 추가

--- Langauge/Java/Reactive/02_Reactor/webfluxTuto/src/main/java/com/example/webfluxtuto/config/Config.java
+++ Langauge/Java/Reactive/02_Reactor/webfluxTuto/src/main/java/com/example/webfluxtuto/config/Config.java
@@ -1,2 +1,17 @@
-package com.example.webfluxtuto.config;public class Config {
+package com.example.webfluxtuto.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class Config implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+
+    }
 }
--- Langauge/Java/Reactive/02_Reactor/webfluxTuto/src/main/java/com/example/webfluxtuto/controller/TestController.java
+++ Langauge/Java/Reactive/02_Reactor/webfluxTuto/src/main/java/com/example/webfluxtuto/controller/TestController.java
@@ -1,2 +1,54 @@
-package com.example.webfluxtuto.controller;public class TestController {
+package com.example.webfluxtuto.controller;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/1")
+    public Mono<List<String>> test(Mono<Param> param) {
+        return Flux.range(1, 500000)
+//                                .publishOn(Schedulers.boundedElastic())
+                                .map(i -> i.toString())
+                                .map(String::toUpperCase)
+                                .map(i -> i + " : test")
+                                .collectList();
+    }
+
+    @GetMapping("/2")
+    public Mono<List<String>> test2(Mono<Param> param) {
+        return param
+//                .subscribeOn(Schedulers.boundedElastic())
+                .flatMap(e -> Flux.range(1, 500000)
+//                        .publishOn(Schedulers.boundedElastic())
+                    .map(i -> i.toString())
+                    .map(String::toUpperCase)
+                    .map(i -> i + " : test")
+                    .collectList()
+                );
+
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class Param {
+        private int[] test;
+
+    }
 }
