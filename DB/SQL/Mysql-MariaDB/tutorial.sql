a8a8bf71a9f6c42c489c38aa3e589de16b3a0a36
yoosc89 <yoosc89@gmail.com>
2024-03-17 22:32:23 +0900

    update

--- DB/SQL/Mysql-MariaDB/tutorial.sql
+++ DB/SQL/Mysql-MariaDB/tutorial.sql
@@ -29,6 +29,7 @@ rollback ;
 
 show variables like 'autocommit'; -- autocommit 설정 확인
 set autocommit = 0; -- auto commit 해제
+set autocommit = 1; -- auto commit 활성화
 
 SELECT GLOBAL STATUS LKIE 'K ey%';
 
@@ -36,3 +37,37 @@ SELECT variables LIKE 'slow_qeury_log%';
 
 
 select sleep(2);
+
+
+CREATE TABLE tab_myisam (id INT not null primary key) ENGINE=MyISAM;
+CREATE TABLE tab_innodb (id INT not null primary key) ENGINE=InnoDB;
+
+insert into tab_myisam values (1);
+insert into tab_innodb values (1);
+
+SELECT * FROM tab_myisam;
+SELECT * FROM tab_innodb;
+
+insert into tab_myisam (id) values (1), (2), (3);
+insert into tab_innodb (id) values (1), (2), (3);
+
+-- innodb_autoinc_lock_mode
+SHOW VARIABLES LIKE 'innodb_autoinc_lock_mode';
+SET INNODB_AUTOINC_LOCK_MODE  = 0; -- 5.0 동일한 잠금 방식 모든 INSERT 문장은 자동 증가락을 사용한다.
+SET INNODB_AUTOINC_LOCK_MODE  = 1; -- 단순히 한 건 또는 여러 건의 레코드를 INSERT할 때 정확히 예측할 수 있는 자동 증가락을 사용할 꼉우 뮤퉥스를 이용해 처리한다.
+SET INNODB_AUTOINC_LOCK_MODE  = 2; -- 뮤텍스를 사용.
+
+
+CREATE TABLE t1_zlib (c1 INT) compression = 'zlib'; -- 테이블 압축
+select * from information_schema.tables where table_name = 't1_zlib'; -- 압축 확인
+
+explain select * from t1_zlib; -- 압축 확인
+
+
+show variables like 'optimizer_switch'; -- 옵티마이저 스위치 확인
+
+select * from information_schema.INNODB_FT_DEFAULT_STOPWORD; -- 기본 불용어 확인
+
+
+
+
