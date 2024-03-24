d535a638e9dbe35c8f3294cf8a6ed52f57b6e371
yoosc89 <yoosc89@gmail.com>
2024-03-24 22:02:53 +0900

    update

--- DB/SQL/Mysql-MariaDB/tutorial.sql
+++ DB/SQL/Mysql-MariaDB/tutorial.sql
@@ -205,3 +205,68 @@ select a.m_id, b.m_id
   from member a
   left join member b on b.m_id = a.m_id -- inner join으로 변경
  where b.m_id in (1, 2, 3, 4);
+
+
+explain analyze -- anti join
+ select a.m_id, b.m_id
+   from member a
+   left join member b on b.m_id = a.m_id
+  where b.m_area is null;
+
+explain analyze
+select a.m_id, b.m_id
+  from member a, member b
+ where b.m_id in (1, 2, 3, 4); -- left join 사용
+
+
+explain
+select * from member a, member b where a.m_id = b.m_id; -- cross join 사용
+
+explain analyze
+select *
+  from member a
+  join lateral ( select *
+                   from member
+                  order by m_id desc
+                  limit 10
+      ) b on a.m_id = b.m_id; -- lateral join 사용
+
+
+explain analyze
+select *
+  from member a
+  join member b on a.m_id = b.m_id
+ order by b.m_id desc limit 10; -- join 사용
+
+
+create schema anamensis;
+
+
+
+CREATE TABLE `role` (
+	`id`	bigint	NOT NULL	COMMENT 'PK',
+	`role`	varchar(20)	NOT NULL	DEFAULT user	COMMENT '권한 정보',
+	`user_pk`	bigint	NOT NULL
+);
+
+CREATE TABLE `login_history` (
+	`id`	bigint	NOT NULL	COMMENT 'PK',
+	`user_pk`	bigint	NOT NULL,
+	`ip`	varchar(255)	NOT NULL	COMMENT '접속 IP주소',
+	`location`	varchar(255)	NOT NULL	COMMENT '접속장소',
+	`device`	varchar(255)	NOT NULL	COMMENT '단말기정보',
+	`create_at`	datetime	NOT NULL	DEFAULT current_timestamp	COMMENT '생성일자'
+);
+
+CREATE TABLE `user` (
+	`id`	bigint	NOT NULL	COMMENT 'PK',
+	`user_id`	varchar(50)	NOT NULL	COMMENT '계정 아이디',
+	`pwd`	varchar(255)	NOT NULL	COMMENT '패스워드',
+	`name`	varchar(100)	NULL	COMMENT '이름',
+	`email`	varchar(255)	NOT NULL	COMMENT '이메일',
+	`phone`	varchar(20)	NULL	COMMENT '핸드폰 번호',
+	`create_at`	datetime	NOT NULL	DEFAULT current_timestamp	COMMENT '생성일자',
+	`update_at`	datetime	NULL	DEFAULT current_timestamp	COMMENT '정보 수정 일자',
+	`is_use`	tinyInt	NOT NULL	DEFAULT 1	COMMENT '계정 사용여부'
+);
+
